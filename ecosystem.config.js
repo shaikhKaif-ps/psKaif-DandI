18d63af3f13fab6af2d6430ba471e4ca3d3f1b05
sk-kaif <kaif.s@parashifttech.com>
2025-12-01 17:10:32 +0530

    ecosystem issue

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,9 @@
 module.exports = {
   apps: [
     {
-      name: 'myapp', // ← Yahan apne app ka naam daal do (jaise: dashboard, web, admin, etc.)
+      name: 'diversity-inclusion', // ← Yahan apne app ka naam daal do (jaise: dashboard, web, admin, etc.)
       script: 'node_modules/next/dist/bin/next', // Next.js ke liye
-      args: 'start', // Next.js production start
+      args: "start -p 3002", // Next.js production start
       cwd: '/home/emergingfromthecave-di-web-dev/htdocs/di-web-dev.emergingfromthecave.net/D-I-Frontend-PS', // ← Yahan exact wahi path daal do jo tumne secrets.APP_DIR mein daala hai
       instances: 'max', // Ya 1 rakh sakte ho chhote VPS mein
       exec_mode: 'cluster', // max instances ke liye cluster mode chahiye
